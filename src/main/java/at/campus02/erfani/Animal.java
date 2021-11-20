4ca62197b4ade26838824ecec9ea708170767b42
Hassan-Erfani <erfani.mojtaba@hotmail.com>
2021-11-20 17:02:19 +0100

    Tests for giveAnimalLoud(), getNameAndAge() & ageCategory() methods implemented

--- src/main/java/at/campus02/erfani/Animal.java
+++ src/main/java/at/campus02/erfani/Animal.java
@@ -1,5 +1,60 @@
 package at.campus02.erfani;
 
 public class Animal {
+private  String name;
+private int age;
+private String animalLoud;
+private String species;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAnimalLoud() {
+        return animalLoud;
+    }
+
+    public void setAnimalLoud(String animalLoud) {
+        this.animalLoud = animalLoud;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String giveAnimalLoud() {
+
+        return ( species.equals("Lion") ) ? "Roarrr" :
+                    species.equals("Dog") ? "Wuff" :
+                        species.equals("Cat") ? "Miau" :
+                            "Please give one of this species 'Dog', 'Lion' or 'Cat'";
+    }
+
+    public String getNameAndage() {
+
+        return "Name: " + name + " Age: " + age;
+    }
+
+    public String ageCategory() {
+       return age > 4 ? "old animal" :
+                age >= 3 ? "young animal" :
+                        age > 0 ? "baby animal" :
+                                "Error";
+    }
 }
--- src/test/java/at/campus02/erfani/AnimalTest.java
+++ src/test/java/at/campus02/erfani/AnimalTest.java
@@ -1,19 +1,53 @@
 package at.campus02.erfani;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnimalTest {
-    int a;
+    Animal animal1;
+    Animal animal2;
+    Animal animal3;
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
-        a = 2;
+
+       animal1 = new Animal();
+       animal1.setName("Hermine");
+       animal1.setAge(1);
+       animal1.setSpecies("Cat");
+
+       animal2 = new Animal();
+       animal2.setName("Tyson");
+       animal2.setAge(3);
+       animal2.setSpecies("Lion");
+
+       animal3 = new Animal();
+       animal3.setName("Johnny");
+       animal3.setAge(5);
+       animal3.setSpecies("Dog");
     }
 
     @Test
-    void myFirstTest() {
-        assertTrue(a > 1);
+    void giveAnimalLoud() {
+        Assertions.assertEquals("Miau", animal1.giveAnimalLoud() );
+        Assertions.assertEquals("Roarrr", animal2.giveAnimalLoud() );
+        Assertions.assertEquals("Wuff", animal3.giveAnimalLoud() );
+    }
+
+    @Test
+    void getNameAge() {
+        Assertions.assertEquals ("Name: Hermine Age: 1", animal1.getNameAndage() );
+        Assertions.assertEquals("Name: Tyson Age: 3", animal2.getNameAndage() );
+        Assertions.assertEquals("Name: Johnny Age: 5", animal3.getNameAndage() );
+    }
+
+    @Test
+    void ageCategory() {
+        Assertions.assertEquals("baby animal", animal1.ageCategory() );
+        Assertions.assertEquals("young animal", animal2.ageCategory() );
+        Assertions.assertEquals("old animal", animal3.ageCategory() );
     }
 }
